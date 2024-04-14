f743fc3433b9cbae0559f3e0c0f942415f2f2223
Alexandre Magno <alexandremagnofs@gmail.com>
2024-04-13 23:06:07 -0300

    fix: comentarios de execucao do script

--- hello.py
+++ hello.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 
+"""Hello World Multi Linguas.
+
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem 
+correspondente.
+
+Como usar:
+
+Tenha a variavel LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+
 # Este programa imprime o Hello World
 print("Hello, World") # Outro comentario
-
-"""
-Comentario
-multi
-linhas
-Python
-"""
